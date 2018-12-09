bb102093facfe15288042bbad901f272cd095c9b
ASVBPREAUBV <paul@paul-mayer.eu>
2018-12-09 16:50:31 +0100

    add cssnext to grunt workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,12 @@
-module.exports = function(grunt) {
+var cssnext = require("postcss-cssnext");
 
+module.exports = function (grunt) {
     grunt.initConfig({
-
         postcss: {
-
             options: {
-
+                processors: [
+                    cssnext()
+                ]
             },
             dist: {
                 src: 'static/css4/style.css',
